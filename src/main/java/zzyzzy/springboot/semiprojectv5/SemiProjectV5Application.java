3d99650c75197d9e190b2a87aa8f48ac666187ec
zzyzigy <zzyzigy@gmail.com>
2023-05-11 12:57:50 +0900

    저장소 초기화 - boot 프로젝트 설정

---
+++ src/main/java/zzyzzy/springboot/semiprojectv5/SemiProjectV5Application.java
@@ -0,0 +1,13 @@
+package zzyzzy.springboot.semiprojectv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiProjectV5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemiProjectV5Application.class, args);
+    }
+
+}
