9bdff16b6ecc0409beb79b15141f4e8e8c609d07
zzyzigy <zzyzigy@gmail.com>
2023-05-11 15:58:52 +0900

    mvc 프로젝트 - lombok 설정

--- src/main/java/zzyzzy/springboot/semiprojectv5/model/Member.java
+++ src/main/java/zzyzzy/springboot/semiprojectv5/model/Member.java
@@ -1,5 +1,8 @@
 package zzyzzy.springboot.semiprojectv5.model;
 
+import lombok.Data;
+
+@Data
 public class Member {
 
     private String mbno;
@@ -15,102 +18,4 @@ public class Member {
     private String phone;
     private String regdate;
 
-    public Member() {
-    }
-
-    public String getMbno() {
-        return mbno;
-    }
-
-    public void setMbno(String mbno) {
-        this.mbno = mbno;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getJumin1() {
-        return jumin1;
-    }
-
-    public void setJumin1(String jumin1) {
-        this.jumin1 = jumin1;
-    }
-
-    public String getJumin2() {
-        return jumin2;
-    }
-
-    public void setJumin2(String jumin2) {
-        this.jumin2 = jumin2;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getAddr1() {
-        return addr1;
-    }
-
-    public void setAddr1(String addr1) {
-        this.addr1 = addr1;
-    }
-
-    public String getAddr2() {
-        return addr2;
-    }
-
-    public void setAddr2(String addr2) {
-        this.addr2 = addr2;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getRegdate() {
-        return regdate;
-    }
-
-    public void setRegdate(String regdate) {
-        this.regdate = regdate;
-    }
 }
--- src/main/java/zzyzzy/springboot/semiprojectv5/model/Zipcode.java
+++ src/main/java/zzyzzy/springboot/semiprojectv5/model/Zipcode.java
@@ -1,5 +1,8 @@
 package zzyzzy.springboot.semiprojectv5.model;
 
+import lombok.Data;
+
+@Data
 public class Zipcode {
 
     private String zipcode;
@@ -9,54 +12,4 @@ public class Zipcode {
     private String ri;
     private String bunji;
 
-    public Zipcode() {
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getSido() {
-        return sido;
-    }
-
-    public void setSido(String sido) {
-        this.sido = sido;
-    }
-
-    public String getGugun() {
-        return gugun;
-    }
-
-    public void setGugun(String gugun) {
-        this.gugun = gugun;
-    }
-
-    public String getDong() {
-        return dong;
-    }
-
-    public void setDong(String dong) {
-        this.dong = dong;
-    }
-
-    public String getRi() {
-        return ri;
-    }
-
-    public void setRi(String ri) {
-        this.ri = ri;
-    }
-
-    public String getBunji() {
-        return bunji;
-    }
-
-    public void setBunji(String bunji) {
-        this.bunji = bunji;
-    }
 }
